76c23ad293005395aba841bf27324103b512c88c
vivek26anand <vivek26anand.va@gmail.com>
2022-05-11 23:02:32 +0530

    fix

--- src/my-team/components/TeamData.js
+++ src/my-team/components/TeamData.js
@@ -1,122 +1,122 @@
-import i1 from '../images/images/i1';
-import i2 from '../images/images/i2';
-import i3 from '../images/images/i3';
-import i4 from '../images/images/i4';
-import i5 from '../images/images/i5';
-import i6 from '../images/images/i6';
-import i7 from '../images/images/i7';
-import i8 from '../images/images/i8';
-import i9 from '../images/images/i9';
-import i10 from '../images/images/i10';
-import i11 from '../images/images/i11';
-import i12 from '../images/images/i12';
-import i13 from '../images/images/i13';
-import i14 from '../images/images/i14';
-import i15 from '../images/images/i15';
-import i16 from '../images/images/i16';
-import i17 from '../images/images/i17';
-import i18 from '../images/images/i18';
-import i19 from '../images/images/i19';
+import i1 from '../images/images/i1.jpg';
+import i2 from '../images/images/i2.jpg';
+import i3 from '../images/images/i3.jpg';
+import i4 from '../images/images/i4.jpg';
+import i5 from '../images/images/i5.jpg';
+import i6 from '../images/images/i6.jpg';
+import i7 from '../images/images/i7.jpg';
+import i8 from '../images/images/i8.jpg';
+import i9 from '../images/images/i9.jpg';
+import i10 from '../images/images/i10.jpg';
+import i11 from '../images/images/i11.jpg';
+import i12 from '../images/images/i12.jpg';
+import i13 from '../images/images/i13.jpg';
+import i14 from '../images/images/i14.jpg';
+import i15 from '../images/images/i15.jpg';
+import i16 from '../images/images/i16.jpg';
+import i17 from '../images/images/i17.jpg';
+import i18 from '../images/images/i18.jpg';
+import i19 from '../images/images/i19.jpg';
 
 const TeamData = [
   {
     id: 1,
     name: "Sanjana Dey",
     profession: "Co-head of Content Wing",
-    img: {i1}
+    img: i1
   },
   {
     id: 2,
     name: "Arkajit Chakraborty ",
     profession: "Media Wing Co-Head",
-    img: {i2}
+    img: i2
   },
   {
     id: 3,
     name: "Shweta Sharma ",
     profession: "Content Wing - Head",
-    img: {i3}
+    img: i3
   },
   {
     id: 4,
     name: "Rohan Jha ",
     profession: "Co-Head of Startup Wing, Member of Content Wing ",
-    img: {i4}
+    img: i4
   },  {
     id: 5,
     name: "Sneha Priya ",
     profession: "Social Media Co-head",
-    img: {i5}
+    img: i5
   },  {
     id: 6,
     name: "Shubhayu Majumdar ",
     profession: "Co-Head Technical Team",
-    img: {i6}
+    img: i6
   },  {
     id: 7,
     name: "Rajeev Tiwari ",
     profession: "Resource Co-Head",
-    img: {i7}
+    img: i7
   },  {
     id: 8,
     name: "Rahul Kumar  ",
     profession: "Admin co-head",
-    img: {i8}
+    img: i8
   },  {
     id: 9,
     name: "DIPANNITA KUNDU ",
     profession: "Social Media Head",
-    img: {i9}
+    img: i9
   },  {
     id: 10,
     name: "Ishika Mondal ",
     profession: "Pr and outreach head",
-    img: {i10}
+    img: i10
   },  {
     id: 11,
     name: "Ayushi Dey ",
     profession: "Management Head",
-    img: {i11}
+    img: i11
   },  {
     id: 12,
     name: "Shristi Rai ",
     profession: "PR and Outreach Co-Head",
-    img: {i12}
+    img: i12
   },  {
     id: 13,
     name: "Kushal Guha ",
     profession: "Management Co-Head ",
-    img: {i13}
+    img: i13
   },  {
     id: 14,
     name: "Nikhil Agarwal  ",
     profession: "Co-Convener and Sponsorship Head",
-    img: {i14}
+    img: i14
   },  {
     id: 15,
     name: "Koustav Banerjee ",
     profession: "Media and Graphics Head, Administration Head",
-    img: {i15}
+    img: i15
   },  {
     id: 16,
     name: "Ankit Kumar Gorain ",
     profession: "Management Co-Head",
-    img: {i16}
+    img: i16
   },  {
     id: 17,
     name: "Ankit Podder",
     profession: "TECH HEAD ",
-    img: {i17}
+    img: i17
   },  {
     id: 18,
     name: "Siladitta ",
     profession: "Co-Head graphic Wing , Head of IIC press wing",
-    img: {i18}
+    img: i18
   },  {
     id: 19,
     name: "Anushka Mukherjee ",
     profession: "Co-Head, Media Wing",
-    img: {i19}
+    img: i19
   }
 ];
 export default TeamData;
