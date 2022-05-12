0b8df46e6c3607ea17e72971effd30cf619cfa2a
Vishal Choubey <74164476+VishalChoubey1019@users.noreply.github.com>
2022-05-12 10:29:19 +0530

    Added New Sponsors

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -1,73 +1,71 @@
-import sponsors2 from "./images/sponsors/2021/sponsor2.png"
-import sponsors1 from "./images/sponsors/2021/sponsor1.png"
-import sponsors3 from"./images/sponsors/2021/sponsor3.png"
-import sponsors4 from"./images/sponsors/2021/sponsor4.png"
-import sponsors5 from"./images/sponsors/2021/sponsor5.png"
-import sponsors6 from"./images/sponsors/2021/sponsor6.png"
-import sponsors7 from"./images/sponsors/2021/sponsor7.png"
-import sponsors8 from"./images/sponsors/2021/sponsor8.png"
-import sponsors9 from"./images/sponsors/2021/sponsor9.jpg"
-import sponsors10 from"./images/sponsors/2021/sponsor10.png"
+import './App.css';
+import cfp from './images/sponsor_images/cfp.png'
+import d2c from './images/sponsor_images/d2c.png'
+import ff from './images/sponsor_images/ff.png'
+import lwt from './images/sponsor_images/lwt.png'
+import sol from './images/sponsor_images/solvoix.png'
 import Footer from "./Footer";
-import Header from "./Header";
+import Header from "./Header"
 
-import React from  "react";
-import "./sponsors_header.css";
-function App2(){
-    return (<>
-              <Header/>
-    <div className="slidetop">
-    <a href="#main"><i className="fas fa-arrow-up"></i></a>
-</div>
-<div id="main" style={{backgroundColor:"Silver"}}>
-    
-    <header className="wrapper">
-        <h1 id="sponsor">SPONSORS</h1>
-        <div className="container">
-            <div className="sponsor_container">
-                <img src={sponsors2} alt=""/>
-                <h2>Supported By</h2>
+// LWT Knowledge Partner
+// Co Founder's Planet Event Partner for Biz-Plan (only) 
+// Powered By Unstop (Formerly D2C)
+// FreeFlow -> Ecosystem partner
+// Solvoix -> Events Partner
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <h1 className='sp-header box threeD'>Sponsors</h1>
+      <div className='sponsors'>
+          <div className='EP'>
+            <h1 className='partner-header'>Events Partner</h1>
+            <div className='flex-box'>        
+              <div className='img-name'>
+                <img className="sp-logos"src = {sol} alt=''/>
+                <p className="sp-name">Solvoix</p>  
+              </div>
+              <div className='img-name'>
+                <img className="sp-logos"src = {cfp} alt=''/>
+                <p className="sp-name">Co Founder's Planet</p>
+              </div>
             </div>
-            <div className="sponsor_container">
-                <img src={sponsors1} alt=""/>
-                <h2>Event Partner</h2>
+          </div>
+
+        <div className='KP'>
+          <h1 className='partner-header'>Knowledge Partner</h1>
+          <div className='flex-box'>
+            <div className='img-name'>
+              <img className="sp-logos"src = {lwt} alt=''/>
+              <p className="sp-name">Learning With Travel</p>
             </div>
-            <div className="sponsor_container">
-                <img src={sponsors3} alt=""/>
-                <h2>Trading Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors4} alt=""/>
-                <h2>Event Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors5} alt=""/>
-                <h2>Platform Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors6} alt=""/>
-                <h2>Startup Travelling Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors7} alt=""/>
-                <h2>Energy Drink Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors8} alt=""/>
-                <h2>Event Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors9} alt=""/>
-                <h2>Event Partner</h2>
-            </div>
-            <div className="sponsor_container">
-                <img src={sponsors10} alt=""/>
-                <h2>Event Partner</h2>
-            </div>
+          </div>
         </div>
-    </header>
-</div>
-    <Footer/>
-    </>)
+
+        <div className='PB'>
+          <h1 className='partner-header'>Powered By</h1>
+          <div className='flex-box'> 
+            <div className='img-name'>
+              <img className="sp-logos"src = {d2c} alt=''/>
+              <p className="sp-name">Unstop (Formerly D2C)</p>
+            </div>    
+          </div>
+        </div>
+
+        <div className='ESP'>
+          <h1 className='partner-header'>Ecosystem Partner</h1>
+          <div className='flex-box'>
+            <div className='img-name'>
+              <img className="sp-logos"src = {ff} alt=''/>
+              <p className="sp-name">FreeFlow Ecosystem partner</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  );
 }
-export default App2
+
+export default App;
