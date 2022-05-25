3ce9659a643cdac83ab3f732f97b71e75f9e88fd
Vishal Choubey <74164476+VishalChoubey1019@users.noreply.github.com>
2022-05-25 08:42:02 +0530

    Added new sponsors

--- src/components/sp-card.js
+++ src/components/sp-card.js
@@ -3,6 +3,14 @@ import d2c from '../sponsor_images/d2c.png'
 import ff from '../sponsor_images/ff.png'
 import lwt from '../sponsor_images/lwt.png'
 import sol from '../sponsor_images/solvoix.png'
+import i22 from '../sponsor_images/22i.png'
+import bkd from '../sponsor_images/bkd.png'
+import bob from '../sponsor_images/bob.png'
+import dsij from '../sponsor_images/dsij.png'
+import wow from '../sponsor_images/wow.png'
+import exc from '../sponsor_images/exc.png'
+import voxit from '../sponsor_images/voxit.png'
+
 import Card from './sp-card-prop'
 
 //phone = name of company
@@ -43,7 +51,7 @@ export default function Spcard(){
                 img = {cfp}
                 width = {240}
                 height = {170}
-                name = "Event Partner"
+                name = "Venture Partner"
                 phone = "Co Founder's Planet"
                 email = "https://www.crunchbase.com/organization/cofounders-planet-pvt-ltd"
             />
@@ -55,6 +63,69 @@ export default function Spcard(){
                 name = "Events Partner"
                 phone = "Solvoix"
                 email = "https://solvoix.com/"
+            />
+
+            <Card
+                img = {bob}
+                width = {240}
+                height = {200}
+                name = "Events Partner"
+                phone = "Bank of Baroda"
+                email = "https://www.bankofbaroda.in/"
+            />
+
+            <Card
+                img = {exc}
+                width = {240}
+                height = {220}
+                name = "Events Partner"
+                phone = "Exchange22"
+                email = "https://exchange22.com/"
+            />
+
+            <Card
+                img = {wow}
+                width = {240}
+                height = {200}
+                name = "Events Partner"
+                phone = "Wow! China"
+                email = "https://www.facebook.com/wowchinabywowmomo/"
+            />
+
+            <Card
+                img = {i22}
+                width = {240}
+                height = {200}
+                name = "Food Sponsor"
+                phone = "2nd Innings"
+                email = "https://www.facebook.com/2ndinnings2019/"
+            />
+
+            <Card
+                img = {dsij}
+                width = {240}
+                height = {200}
+                name = "Events Partner"
+                phone = "Dalal Street Investment..."
+                email = "https://www.dsij.in/"
+            />
+
+            <Card
+                img = {bkd}
+                width = {260}
+                height = {200}
+                name = "Events Partner"
+                phone = "BoookD.com"
+                email = "https://boookd.com/"
+            />
+
+            <Card
+                img = {voxit}
+                width = {240}
+                height = {200}
+                name = "Events Partner"
+                phone = "Voxit Media Tech"
+                email = "https://voxitworld.com/"
             />
             
         </>
